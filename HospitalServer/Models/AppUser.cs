37264b017ae67f123bc0837a0f5145b6f8a81922
realjesse <154276383+realjesse@users.noreply.github.com>
2026-06-13 19:25:02 -0700

    Create AppUser

---
+++ HospitalServer/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿// Represents the user object stored in MongoDb.
+namespace HospitalServer.Models
+{
+    public class AppUser
+    {
+        [MongoDB.Bson.Serialization.Attributes.BsonId]
+        [MongoDB.Bson.Serialization.Attributes.BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
