1b8b3493410c865a13e8c6198a0789155a59f127
realjesse <154276383+realjesse@users.noreply.github.com>
2026-06-13 20:49:03 -0700

    Create Login form and client functionality

--- HospitalClient/Form1.Designer.cs
+++ HospitalClient/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.goToRegisterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(113, 76);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 0;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(113, 115);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.passwordTextBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(113, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(113, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(126, 153);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // goToRegisterButton
+            // 
+            this.goToRegisterButton.Location = new System.Drawing.Point(33, 28);
+            this.goToRegisterButton.Name = "goToRegisterButton";
+            this.goToRegisterButton.Size = new System.Drawing.Size(75, 23);
+            this.goToRegisterButton.TabIndex = 5;
+            this.goToRegisterButton.Text = "button2";
+            this.goToRegisterButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(327, 230);
+            this.Controls.Add(this.goToRegisterButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.usernameTextBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button goToRegisterButton;
     }
 }
 
--- HospitalClient/Form1.cs
+++ HospitalClient/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using HospitalClient.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,41 @@ namespace HospitalClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private async Task loginButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var loginRequest = new LoginRequest
+                {
+                    Username = usernameTextBox.Text,
+                    Password = passwordTextBox.Text
+                };
+
+                var json = JsonConvert.SerializeObject(loginRequest);
+
+                var content = new StringContent(
+                    json,
+                    Encoding.UTF8,
+                    "application/json"
+                 );
+
+
+                var httpClient = new HttpClient();
+
+                var response = await httpClient.PostAsync("http://localhost:5220/api/auth/login", content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    MessageBox.Show("Login failed");
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
         }
     }
 }
