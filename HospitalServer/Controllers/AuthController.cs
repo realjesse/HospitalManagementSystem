225fe1a0e122ca2313850439d9c874dc80e0fb34
realjesse <154276383+realjesse@users.noreply.github.com>
2026-06-13 20:00:49 -0700

    Create AuthController

---
+++ HospitalServer/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using HospitalServer.DTOs;
+using HospitalServer.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalServer.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        // Holds connection data for MongoDB and provides methods.
+        public AuthController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        // Controls when a user registers.
+        [HttpGet("register")]
+        public async Task<ActionResult<AuthResponse>> Register(RegisterRequest request)
+        {
+            var result = await _userService.RegisterAsync(request);
+
+            if (!result.Success)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);
+        }
+
+        // Controls when a user logins in.
+        [HttpGet("login")]
+        public async Task<ActionResult<AuthResponse>> Login(LoginRequest request)
+        {
+            var result = await _userService.LoginAsync(request);
+
+            if (!result.Success)
+            {
+                return Unauthorized(result);
+            }
+
+            return Ok(result);
+        }
+    }
+}
