22cd5b2b0074c7ee3279aba391443b310447db25
realjesse <154276383+realjesse@users.noreply.github.com>
2026-06-19 13:50:32 -0700

    Create PatientResponse, PatientRequest DTOs

---
+++ HospitalServer/DTOs/PatientRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace HospitalServer.DTOs
+{
+    public class PatientRequest
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ HospitalServer/DTOs/PatientResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalServer.DTOs
+{
+    public class PatientResponse
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime DateOfBirth { get; set; }
+    }
+}
